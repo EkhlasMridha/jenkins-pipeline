103f12683537f8cb7227f9aec23e777353a76ff3
= <=>
2020-10-17 20:01:54 +0600

    update

--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RootRoutingModule } from './root-routing/root-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RootRoutingModule],
+  imports: [BrowserModule, RootRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
