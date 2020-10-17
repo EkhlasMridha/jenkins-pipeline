d32a0db4d2aab9bc45ae1a90c2f8fc79ba950161
= <=>
2020-10-17 23:21:27 +0600

    complete

--- src/app/app-filter/components/filter/filter.component.ts
+++ src/app/app-filter/components/filter/filter.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
-
+  tagList: string[] = ["Covid19", "SDG's", "Digital Healthcare", "Cyber Security", "Food Tech", "Solar Energy", "Agriculture", "Wildfire", "Bio Fuel", "Drone", "Waste Management"];
   constructor() { }
 
   ngOnInit(): void {
